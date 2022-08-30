58be7f201bd9d02d6639a002e0efddfa8e17e589
zombified_patato <bryanbelle2003@gmail.com>
2022-08-30 17:31:25 +0200

    Initial Commit.

---
+++ src/main/java/com/github/ZombifiedPatato/explorer_world/Main.java
@@ -0,0 +1,19 @@
+package com.github.ZombifiedPatato.explorer_world;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static String MOD_ID = "explorer_world";
+	public static final Logger LOGGER = LoggerFactory.getLogger("explorer_world");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello from Explorer World!");
+	}
+}
